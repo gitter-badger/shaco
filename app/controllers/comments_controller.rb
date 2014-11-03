8b3852ec3a96684b494c1e88dc82b6fb68c8a71b
narikatel <my.box686@gmail.com>
2014-11-03 20:46:54 +0500

    Создание и настрйока девайся и настройка ассоциациий

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :find_article, only: [:create, :new, :destroy]
+
+  def new
+    @comment = Comment.new
+  end
+
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.article = @article
+    if @comment.save
+      redirect_to article_path(@comment.article)
+    else
+      render 'form'
+    end
   end
 
   def destroy
@@ -14,6 +25,10 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body)
+    end
+
+    def find_article
+      @article = Article.find(params[:article_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Blog::Application.routes.draw do
   resources :articles do
     resources :comments
   end
-  root 'welcome#index'
+  root 'articles#index'
 end
